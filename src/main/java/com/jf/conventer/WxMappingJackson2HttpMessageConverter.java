9f525feabd018a23fc2a6a30e77acab2ec2baab1
江峰 <jiangfeng@smarchit.com>
2020-01-15 13:23:57 +0800

    initial commit

--- src/main/java/com/jf/conventer/WxMappingJackson2HttpMessageConverter.java
+++ src/main/java/com/jf/conventer/WxMappingJackson2HttpMessageConverter.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @create 2020-01-14   20:41
  */
 public class WxMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
-    public WxMappingJackson2HttpMessageConverter(){
+    public WxMappingJackson2HttpMessageConverter() {
         List<MediaType> mediaTypes = new ArrayList<>();
         mediaTypes.add(MediaType.TEXT_PLAIN);
         mediaTypes.add(MediaType.TEXT_HTML);
